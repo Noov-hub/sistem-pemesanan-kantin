6853afe4d42e9c826ea7ca8da536b19e225030c0
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-11-24 23:39:00 +0700

    lengkapi routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
 
-// POST http://localhost:5000/api/orders
+// CREATE: POST http://localhost:5000/api/orders
 router.post('/', orderController.createOrder);
+
+// READ: GET http://localhost:5000/api/orders
+router.get('/', orderController.getAllOrders);
+
+// UPDATE: PUT http://localhost:5000/api/orders/:id
+// Contoh: http://localhost:5000/api/orders/5
+router.put('/:id', orderController.updateOrderStatus);
+
+// DELETE: DELETE http://localhost:5000/api/orders/:id
+router.delete('/:id', orderController.deleteOrder);
 
 module.exports = router;
