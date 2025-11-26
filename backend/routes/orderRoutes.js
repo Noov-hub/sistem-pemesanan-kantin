f5d889c49bcac9e121c105c7d140152919afa1f4
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-11-26 19:38:58 +0700

    routes dgn token dan public

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
+const { verifyToken } = require('../middleware/authMiddleware');
 
 // --- POST (Create) ---
 router.post('/', orderController.createOrder); // Buat Pesanan
-
+router.get('/queue', orderController.getPublicQueue); // URL: /api/orders/queue
 // --- GETTERS (Filter Khusus) ---
-router.get('/', orderController.getAllOrders);          // URL: /api/orders
-router.get('/active', orderController.getActiveOrders); // URL: /api/orders/active
-router.get('/kitchen', orderController.getKitchenOrders); // URL: /api/orders/kitchen
-router.get('/new', orderController.getNewOrders);       // URL: /api/orders/new
-router.get('/history', orderController.getFinishedOrders); // URL: /api/orders/history
+
+router.get('/', verifyToken, orderController.getAllOrders);// URL: /api/orders
+router.get('/active', verifyToken, orderController.getActiveOrders);// URL: /api/orders/active
+router.get('/kitchen', verifyToken, orderController.getKitchenOrders);// URL: /api/orders/kitchen
+router.get('/new', verifyToken, orderController.getNewOrders);// URL: /api/orders/new
+router.get('/history', verifyToken, orderController.getFinishedOrders);// URL: /api/orders/history
 
 // --- PUT & DELETE (By ID) ---
 // Wajib ditaruh PALING BAWAH agar tidak bentrok dengan route di atas
-router.put('/:id', orderController.updateOrderStatus);
-router.delete('/:id', orderController.deleteOrder);
+router.put('/:id', verifyToken, orderController.updateOrderStatus);
+router.delete('/:id', verifyToken, orderController.deleteOrder);
 
 module.exports = router;
