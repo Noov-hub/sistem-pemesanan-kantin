a35a4043446e164d5a4ff4af1235fb36d940c79c
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-11-26 20:12:55 +0700

    add axios.js

---
+++ frontend/lib/axios.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:5000/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// --- INTERCEPTOR (Middleware Frontend) ---
+// Setiap kali Axios mau kirim request, fungsi ini jalan duluan
+api.interceptors.request.use(
+    (config) => {
+        // Ambil token dari penyimpanan browser
+        const token = localStorage.getItem('token');
+        
+        // Jika ada token, tempelkan ke Header Authorization
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
