a29a984f224b50aa5e560a27821962c111d14a24
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-11-27 16:36:00 +0700

    refactor: pindah route untuk create user ke adminRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,8 +6,4 @@ const { verifyToken, verifyAdmin } = require('../middleware/authMiddleware');
 // Route PUBLIK (Siapa saja boleh akses)
 router.post('/login', authController.login);
 
-// Route PROTECTED (Harus Login & Harus Admin)
-// "verifyToken" cek login dulu, baru "verifyAdmin" cek role
-router.post('/create', verifyToken, verifyAdmin, authController.createUser);
-
 module.exports = router;
