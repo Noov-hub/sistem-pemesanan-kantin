d890f4f8c19e7cb6b0bd73db75984c23fb032e6d
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-12-18 12:23:09 +0700

    fix : implement new structure of database

--- backend/kantin_db.sql
+++ backend/kantin_db.sql
@@ -1,52 +1,54 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : LatihanDatabase
-Source Server Version : 80030
-Source Host           : localhost:3306
-Source Database       : kantin_db
+ Source Server         : LatihanDatabase
+ Source Server Type    : MySQL
+ Source Server Version : 80030 (8.0.30)
+ Source Host           : localhost:3306
+ Source Schema         : kantin_db
 
-Target Server Type    : MYSQL
-Target Server Version : 80030
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 80030 (8.0.30)
+ File Encoding         : 65001
 
-Date: 2025-11-25 17:34:36
+ Date: 18/12/2025 12:22:34
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
 -- ----------------------------
--- Table structure for `orders`
+-- Table structure for orders
 -- ----------------------------
 DROP TABLE IF EXISTS `orders`;
-CREATE TABLE `orders` (
+CREATE TABLE `orders`  (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int DEFAULT NULL,
-  `customer_name` varchar(100) NOT NULL,
-  `order_notes` text NOT NULL,
-  `status` enum('new','confirmed','cooking','ready','completed','cancelled') DEFAULT 'new',
-  `total_amount` decimal(10,2) DEFAULT '0.00',
-  `payment_status` enum('unpaid','paid','refunded') DEFAULT 'unpaid',
+  `user_id` int NULL DEFAULT NULL,
+  `customer_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `order_notes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `status` enum('new','confirmed','cooking','ready','completed','cancelled') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT 'new',
+  `total_amount` decimal(10, 2) NULL DEFAULT 0.00,
+  `payment_status` enum('unpaid','paid','refunded') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT 'unpaid',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `confirmed_at` timestamp NULL DEFAULT NULL,
+  `cooking_at` timestamp NULL DEFAULT NULL,
+  `ready_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for `users`
+-- Table structure for users
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
+CREATE TABLE `users`  (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` enum('cashier','kitchen','customer','admin') DEFAULT 'cashier',
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `role` enum('cashier','kitchen','customer','admin') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT 'cashier',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `username`(`username` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES ('1', 'kasir1', 'admin123', 'cashier', '2025-11-24 19:04:26');
-INSERT INTO `users` VALUES ('2', 'dapur1', 'admin123', 'kitchen', '2025-11-24 19:04:26');
+SET FOREIGN_KEY_CHECKS = 1;
