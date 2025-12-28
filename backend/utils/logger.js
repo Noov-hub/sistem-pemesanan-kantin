c5b6e90412038a57e91542036da261325f43a8a6
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-12-29 00:20:42 +0700

    fix: menambah bagian socket dan tambah parameter role dan id pada createLog

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,12 +1,28 @@
 const db = require('../config/db');
 
-exports.createLog = async (userId, action, targetId, detail) => {
+exports.createLog = async (userId, action, targetId, details, io, role, ip) => {
     try{
-        await db.execute(
-            "INSERT INTO activity_logs (user_id, action, target_id, details) VALUES (?, ?, ?, ?)",
-            [userId, action, targetId, detail]
+        const [result] = await db.execute(
+            "INSERT INTO activity_logs (user_id, action, target_id, details, ip_address) VALUES (?, ?, ?, ?, ?)",
+            [userId, action, targetId, details, ip]
         );
-        console.log(`[LOG]: ${action} by user ${userId}`);
+
+        const extractedUsername = details.split(' ')[1] || 'Guest';
+
+        if(io){
+            io.emit('new_log', {
+                id: result.insertId,
+                username: extractedUsername,
+                user_id: userId,
+                role: role || "staff",
+                ip: ip,
+                action,
+                target_id: targetId,
+                details,
+                created_at: new Date()
+            });
+        }
+        console.log(`[LOG]: ${action} by user ${extractedUsername}`);
     }catch (error){
         console.error("Gagal membuat Log: ", error.message);
     }
