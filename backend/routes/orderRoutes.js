56579f54511c8f4e433ac923f2b0eda7e8599b13
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-12-29 19:11:11 +0700

    feat: menambahkan routes untuk tab ready

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -12,6 +12,7 @@ router.get('/', verifyToken, orderController.getAllOrders);// URL: /api/orders
 router.get('/active', verifyToken, orderController.getActiveOrders);// URL: /api/orders/active
 router.get('/kitchen', verifyToken, orderController.getKitchenOrders);// URL: /api/orders/kitchen
 router.get('/new', verifyToken, orderController.getNewOrders);// URL: /api/orders/new
+router.get('/ready', verifyToken, orderController.getReadyOrders);// URL: /api/orders/ready
 router.get('/history', verifyToken, orderController.getFinishedOrders);// URL: /api/orders/history
 router.put('/:id/confirm', verifyToken, orderController.confirmOrder);
 // --- PUT & DELETE (By ID) ---
